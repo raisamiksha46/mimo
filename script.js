7ce974c9b370ce51a666ea1b26b668a1eb044236
Samiksha Rai <62441596+raisamiksha46@users.noreply.github.com>
2024-11-13 01:50:43 +0530

    Javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+const sentence = "Hey there";
+console.log(sentence);
+
+
+const var1 = 12;
+const var2 = 14;
+const var3 = var1 + var2;
+console.log(var3);
+
+
+
+let jimage = 12;
+let dwightage = 16;
+let pamage = 10;
+let michaelage = jimage + pamage;
+console.log(dwightage > jimage);
+console.log(
+ "dwight is older bro to jim");
+const result = (michaelage >
+ dwightage && jimage >
+ pamage)
+console.log(result);
+console.log(
+ "Michael is an uncle " + "007");
